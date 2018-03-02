d24257f872cdae6c667fced9b0eaa668413e5c7e
Unknown <damien.georges2@gmail.com>
2018-03-02 11:34:57 +0100

    add description to install-dependencies.R script

--- pracs/install-dependencies.R
+++ pracs/install-dependencies.R
@@ -1,3 +1,24 @@
+##' ---
+##' title: Install SPE practicals R dependencies
+##' author: Damien G.
+##' ---
+
+##' This script will screen all the files from the working directory (and childs) to detect the 
+##' required packages and will then install them.
+##' 
+##' If you run the script without argument, the latest stable version of packages (and dependencies) 
+##' will be installed (default)
+##' 
+##' `Rscript install-dependencies.R`
+##' 
+##' Alternatively giving the script a date argument (as `YYYY-MM-DD`) will lead to the installation
+##' of the packages et this date (threw `checkpoint` features). This can be usefull or reproductibility
+##' concerns.
+##' 
+##' `Rscript install-dependencies.R 2018-01-01`
+##' 
+
+## read script arguments if some given
 args <- commandArgs(trailingOnly = TRUE) 
 
 ## get the list of available package
@@ -28,8 +49,8 @@ if(!length(args)){
 
 quit(save = 'no', status = 0) 
 
-## the bash version
-# ## install the packages at a defined checkpoint date =============================================== 
+# ### the formal bash version ======================================================================
+# ## install the packages at a defined checkpoint date  
 # 
 # # define the checkpoint date 
 # CHECKPOINT_DATE=2018-02-01 
@@ -38,4 +59,7 @@ quit(save = 'no', status = 0)
 # # set env var to let R know where the pkg are installed (trick based on Epi pkg location) 
 # EPI_PKG_LOCATION=`find .checkpoint/$CHECKPOINT_DATE/ -type d -name "Epi" -print` 
 # R_LIBS_USER=${EPI_PKG_LOCATION%%/Epi} 
+#
+
+
 # export R_LIBS_USER 
