bcd8437ed3e9a9babf18d7d8e482c0e9c8364f4b
martynplummer <martyn.plummer@gmail.com>
2018-05-30 17:32:47 +0200

    Updating R History and Ecology slides

--- lectures/ecology/figures/top-cran.R
+++ lectures/ecology/figures/top-cran.R
@@ -1,12 +1,28 @@
-x <- read.table("top-cran.csv", header=TRUE, sep=",", as.is=FALSE)
-N <- nrow(x)
+library(cranlogs)
+N <- 20
+x <- cran_top_downloads("last-month", N)
 x <- x[N:1,]
+
+##Identify which packages are in the tidyverse
+tidyverse.imports <- available.packages()["tidyverse","Imports"]
+in.tidyverse <- logical(N)
+for (i in 1:N) {
+    in.tidyverse[i] <- grepl(x$package[i], tidyverse.imports)
+}
+
 pch <- 19
-png("top-cran.png", width=6, height=6, units="in", res=100)
-dotchart(x$downloads/1000, x$name,
-         col=c(hadley="blue",graphics="red",other="black")[x$type],
-         xlab="Downloads Jan-May 2015 (thousands)", pch=pch, fg="yellow",
-         xlim=c(0, 800))
-legend("bottomright", col=c("red","blue", "black"), pch=pch,
-       legend=c("Hadleyverse", "graphics", "other"), bg="white")
+x$count <- x$count/1000
+png("top20-cran.png", width=6, height=6, units="in", res=100)
+dotchart(x$count, x$package,
+         col=ifelse(in.tidyverse, "blue", "black"),
+         xlab=paste("Downloads", format(Sys.time(), "%b %Y"), "(thousands)"),
+         pch=pch, fg="yellow", xlim=c(0, max(x$count)))
+legend("bottomright", col=c("blue","black"), pch=pch,
+       legend=c("Tidyverse", "other"), bg="white")
+dev.off()
+
+png("top100-cran.png", width=6, height=6, units="in", res=100)
+y <- cran_top_downloads("last-month", 100)
+plot(y$count/1000, type="h", ylim=c(0, max(y$count/1000)), lwd=2,
+     ylab="Downloads (thousands)", xlab="rank")
 dev.off()
