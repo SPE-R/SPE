d9e702f2892272315c9b748c9198c015fbc0e730
martynplummer <martyn.plummer@gmail.com>
2018-06-05 16:26:32 +0200

    Update figures for R History and Ecology lecture

--- lectures/ecology/figures/top-cran.R
+++ lectures/ecology/figures/top-cran.R
@@ -19,10 +19,12 @@ dotchart(x$count, x$package,
          pch=pch, fg="yellow", xlim=c(0, max(x$count)))
 legend("bottomright", col=c("blue","black"), pch=pch,
        legend=c("Tidyverse", "other"), bg="white")
+title("Top 20 packages by downloads")
 dev.off()
 
 png("top100-cran.png", width=6, height=6, units="in", res=100)
 y <- cran_top_downloads("last-month", 100)
 plot(y$count/1000, type="h", ylim=c(0, max(y$count/1000)), lwd=2,
      ylab="Downloads (thousands)", xlab="rank")
+title("Top 100 packages by downloads")
 dev.off()
