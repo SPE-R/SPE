2548f4fb1d5e84c0b40c135a7c94e437897d451e
DamienGeorges <damien.georges2@gmail.com>
2023-05-30 09:13:03 +0000

    Update gh-spe-material to output generated at e2aa095

--- Rsolutions/DMDK-s.R
+++ Rsolutions/DMDK-s.R
@@ -95,14 +95,149 @@ summary(LL)
 
 
 ###################################################
-### code chunk number 11: DMDK-s.rnw:312-314
+### code chunk number 11: DMDK-s.rnw:243-247
+###################################################
+r.m <- gam(cbind(lex.Xst == "Dead", lex.dur) ~ s(A, k = 20),
+            family = poisreg,
+              data = subset(SL, sex == "M"))
+r.f <- update(r.m, data = subset(SL, sex == "F"))
+
+
+###################################################
+### code chunk number 12: DMDK-s.rnw:257-259
+###################################################
+r.m <- gam.Lexis(subset(SL, sex == "M"), ~ s(A, k = 20))
+r.f <- gam.Lexis(subset(SL, sex == "F"), ~ s(A, k = 20))
+
+
+###################################################
+### code chunk number 13: DMDK-s.rnw:304-308
+###################################################
+nd <-  data.frame(A = seq(20, 80, 0.5))
+p.m <- ci.pred(r.m, newdata = nd)
+p.f <- ci.pred(r.f, newdata = nd)
+head(p.m)
+
+
+###################################################
+### code chunk number 14: a-rates
+###################################################
+matplot(nd$A, cbind(p.m, p.f) * 1000,
+        type = "l", col = rep(c("blue","red"),each = 3), lwd = c(3,1,1), lty = 1,
+        log = "y", xlab = "Age", ylab = "Mortality of DM ptt per 1000 PY")
+
+
+###################################################
+### code chunk number 15: A-rates
+###################################################
+matshade(nd$A, cbind(p.m,p.f) * 1000, plot = TRUE,
+         col = c("blue","red"), lty = 1, lwd = 3,
+         log = "y", xlab = "Age", ylab = "Mortality among DM ptt per 1000 PY")
+
+
+###################################################
+### code chunk number 16: DMDK-s.rnw:378-388
+###################################################
+Mcr <- gam.Lexis(subset(SL, sex == "M"),
+                 ~ s(  A, bs = "cr", k = 10) +
+                   s(  P, bs = "cr", k = 10) +
+                   s(dur, bs = "cr", k = 10))
+summary(Mcr)
+Fcr <- gam.Lexis(subset(SL, sex == "F"),
+                 ~ s(  A, bs = "cr", k = 10) +
+                   s(  P, bs = "cr", k = 10) +
+                   s(dur, bs = "cr", k = 10))
+summary(Fcr)
+
+
+###################################################
+### code chunk number 17: plgam-default
+###################################################
+par(mfcol = c(3,2))
+plot(Fcr, ylim = c(-3,3), col = "red")
+plot(Mcr, ylim = c(-3,3), col = "blue",
+          lwd = 2, shade = TRUE, shade.col = adjustcolor("blue", alpha = 0.15))
+
+
+###################################################
+### code chunk number 18: DMDK-s.rnw:426-428
+###################################################
+anova(Mcr, r.m, test = "Chisq")
+anova(Fcr, r.f, test = "Chisq")
+
+
+###################################################
+### code chunk number 19: DMDK-s.rnw:480-485
+###################################################
+pts <- seq(0, 12, 1/4)
+nd <- data.frame(A =   50 + pts,
+                 P = 1995 + pts,
+               dur =        pts)
+head(cbind(nd$A, ci.pred(Mcr, newdata = nd) * 1000))
+
+
+###################################################
+### code chunk number 20: rates
+###################################################
+plot(NA, xlim = c(50, 85), ylim = c(5, 400), log = "y",
+         xlab = "Age", ylab = "Mortality rate for DM patients")
+for(ip in c(1995, 2005))
+for(ia in c(50, 60, 70))
+   {
+nd <- data.frame(A = ia + pts,
+                 P = ip + pts,
+               dur =      pts)
+matshade(nd$A, ci.pred(Mcr, nd) * 1000, col = "blue", lty = 1 + (ip == 1995))
+matshade(nd$A, ci.pred(Fcr, nd) * 1000, col = "red" , lty = 1 + (ip == 1995))
+   }
+
+
+###################################################
+### code chunk number 21: rates5
+###################################################
+Mcr <- gam.Lexis(subset(SL, sex == "M"),
+                 ~ s(  A, bs = "cr", k = 10) +
+                   s(  P, bs = "cr", k = 10) +
+                   s(dur, bs = "cr", k = 5))
+summary(Mcr)
+Fcr <- gam.Lexis(subset(SL, sex == "F"),
+                 ~ s(  A, bs = "cr", k = 10) +
+                   s(  P, bs = "cr", k = 10) +
+                   s(dur, bs = "cr", k = 5))
+summary(Fcr)
+gam.check(Mcr)
+gam.check(Fcr)
+
+
+###################################################
+### code chunk number 22: rates-5
+###################################################
+plot(NA, xlim = c(50,80), ylim = c(0.9,100), log = "y",
+         xlab = "Age", ylab = "Mortality rate for DM patients")
+abline(v = c(50,55,60,65,70), col = gray(0.8))
+# for(ip in c(1995,2005))
+ip <- 2005
+for(ia in seq(50, 70, 5))
+   {
+nd <- data.frame(A = ia + pts,
+                 P = ip + pts,
+               dur =      pts)
+matshade(nd$A, rm <- ci.pred(Mcr, nd) * 1000, col = "blue", lwd = 2)
+matshade(nd$A, rf <- ci.pred(Fcr, nd) * 1000, col = "red" , lwd = 2)
+matshade(nd$A, ci.ratio(rm, rf), lwd = 2)
+   }
+abline(h = 1, lty = "55")
+
+
+###################################################
+### code chunk number 23: DMDK-s.rnw:690-692
 ###################################################
 SL$Am <- floor(SL$A + 0.25)
 SL$Pm <- floor(SL$P + 0.25)
 
 
 ###################################################
-### code chunk number 12: DMDK-s.rnw:318-326
+### code chunk number 24: DMDK-s.rnw:696-704
 ###################################################
 data(M.dk)
 str(M.dk)
@@ -115,7 +250,7 @@ str(M.dk)
 
 
 ###################################################
-### code chunk number 13: DMDK-s.rnw:331-334
+### code chunk number 25: DMDK-s.rnw:709-712
 ###################################################
 SLr <- merge(SL, M.dk[,c("sex", "Am", "Pm", "rate")])
 dim(SL)
@@ -123,7 +258,7 @@ dim(SLr)
 
 
 ###################################################
-### code chunk number 14: DMDK-s.rnw:343-346
+### code chunk number 26: DMDK-s.rnw:721-724
 ###################################################
 SLi <- inner_join(SL, M.dk[,c("sex","Am","Pm","rate")])
 dim(SL)
@@ -131,7 +266,7 @@ dim(SLi)
 
 
 ###################################################
-### code chunk number 15: DMDK-s.rnw:359-377
+### code chunk number 27: DMDK-s.rnw:737-755
 ###################################################
 SLr$E <- SLr$lex.dur * SLr$rate / 1000
 stat.table(sex,
@@ -154,7 +289,7 @@ stat.table(list(sex,
 
 
 ###################################################
-### code chunk number 16: DMDK-s.rnw:399-403
+### code chunk number 28: DMDK-s.rnw:777-781
 ###################################################
 msmr <- glm((lex.Xst == "Dead") ~ sex - 1 + offset(log(E)),
             family = poisson,
@@ -163,7 +298,7 @@ ci.exp(msmr)
 
 
 ###################################################
-### code chunk number 17: DMDK-s.rnw:427-431
+### code chunk number 29: DMDK-s.rnw:805-809
 ###################################################
 msmr <- glm(cbind(lex.Xst == "Dead", E) ~ sex - 1,
             family = poisreg,
@@ -172,147 +307,12 @@ ci.exp(msmr)
 
 
 ###################################################
-### code chunk number 18: DMDK-s.rnw:435-439
+### code chunk number 30: DMDK-s.rnw:813-817
 ###################################################
 (CM <- rbind(M = c(1,0),
              W = c(0,1),
          'M/F' = c(1,-1)))
 round(ci.exp(msmr, ctr.mat = CM), 2)
-
-
-###################################################
-### code chunk number 19: DMDK-s.rnw:477-481
-###################################################
-r.m <- gam(cbind(lex.Xst == "Dead", lex.dur) ~ s(A, k = 20),
-            family = poisreg,
-              data = subset(SL, sex == "M"))
-r.f <- update(r.m, data = subset(SL, sex == "F"))
-
-
-###################################################
-### code chunk number 20: DMDK-s.rnw:491-493
-###################################################
-r.m <- gam.Lexis(subset(SL, sex == "M"), ~ s(A, k = 20))
-r.f <- gam.Lexis(subset(SL, sex == "F"), ~ s(A, k = 20))
-
-
-###################################################
-### code chunk number 21: DMDK-s.rnw:538-542
-###################################################
-nd <-  data.frame(A = seq(20, 80, 0.5))
-p.m <- ci.pred(r.m, newdata = nd)
-p.f <- ci.pred(r.f, newdata = nd)
-head(p.m)
-
-
-###################################################
-### code chunk number 22: a-rates
-###################################################
-matplot(nd$A, cbind(p.m, p.f) * 1000,
-        type = "l", col = rep(c("blue","red"),each = 3), lwd = c(3,1,1), lty = 1,
-        log = "y", xlab = "Age", ylab = "Mortality of DM ptt per 1000 PY")
-
-
-###################################################
-### code chunk number 23: A-rates
-###################################################
-matshade(nd$A, cbind(p.m,p.f) * 1000, plot = TRUE,
-         col = c("blue","red"), lty = 1, lwd = 3,
-         log = "y", xlab = "Age", ylab = "Mortality among DM ptt per 1000 PY")
-
-
-###################################################
-### code chunk number 24: DMDK-s.rnw:612-622
-###################################################
-Mcr <- gam.Lexis(subset(SL, sex == "M"),
-                 ~ s(  A, bs = "cr", k = 10) +
-                   s(  P, bs = "cr", k = 10) +
-                   s(dur, bs = "cr", k = 10))
-summary(Mcr)
-Fcr <- gam.Lexis(subset(SL, sex == "F"),
-                 ~ s(  A, bs = "cr", k = 10) +
-                   s(  P, bs = "cr", k = 10) +
-                   s(dur, bs = "cr", k = 10))
-summary(Fcr)
-
-
-###################################################
-### code chunk number 25: plgam-default
-###################################################
-par(mfcol = c(3,2))
-plot(Fcr, ylim = c(-3,3), col = "red")
-plot(Mcr, ylim = c(-3,3), col = "blue",
-          lwd = 2, shade = TRUE, shade.col = adjustcolor("blue", alpha = 0.15))
-
-
-###################################################
-### code chunk number 26: DMDK-s.rnw:660-662
-###################################################
-anova(Mcr, r.m, test = "Chisq")
-anova(Fcr, r.f, test = "Chisq")
-
-
-###################################################
-### code chunk number 27: DMDK-s.rnw:714-719
-###################################################
-pts <- seq(0, 12, 1/4)
-nd <- data.frame(A =   50 + pts,
-                 P = 1995 + pts,
-               dur =        pts)
-head(cbind(nd$A, ci.pred(Mcr, newdata = nd) * 1000))
-
-
-###################################################
-### code chunk number 28: rates
-###################################################
-plot(NA, xlim = c(50, 85), ylim = c(5, 400), log = "y",
-         xlab = "Age", ylab = "Mortality rate for DM patients")
-for(ip in c(1995, 2005))
-for(ia in c(50, 60, 70))
-   {
-nd <- data.frame(A = ia + pts,
-                 P = ip + pts,
-               dur =      pts)
-matshade(nd$A, ci.pred(Mcr, nd) * 1000, col = "blue", lty = 1 + (ip == 1995))
-matshade(nd$A, ci.pred(Fcr, nd) * 1000, col = "red" , lty = 1 + (ip == 1995))
-   }
-
-
-###################################################
-### code chunk number 29: rates5
-###################################################
-Mcr <- gam.Lexis(subset(SL, sex == "M"),
-                 ~ s(  A, bs = "cr", k = 10) +
-                   s(  P, bs = "cr", k = 10) +
-                   s(dur, bs = "cr", k = 5))
-summary(Mcr)
-Fcr <- gam.Lexis(subset(SL, sex == "F"),
-                 ~ s(  A, bs = "cr", k = 10) +
-                   s(  P, bs = "cr", k = 10) +
-                   s(dur, bs = "cr", k = 5))
-summary(Fcr)
-gam.check(Mcr)
-gam.check(Fcr)
-
-
-###################################################
-### code chunk number 30: rates-5
-###################################################
-plot(NA, xlim = c(50,80), ylim = c(0.9,100), log = "y",
-         xlab = "Age", ylab = "Mortality rate for DM patients")
-abline(v = c(50,55,60,65,70), col = gray(0.8))
-# for(ip in c(1995,2005))
-ip <- 2005
-for(ia in seq(50, 70, 5))
-   {
-nd <- data.frame(A = ia + pts,
-                 P = ip + pts,
-               dur =      pts)
-matshade(nd$A, rm <- ci.pred(Mcr, nd) * 1000, col = "blue", lwd = 2)
-matshade(nd$A, rf <- ci.pred(Fcr, nd) * 1000, col = "red" , lwd = 2)
-matshade(nd$A, ci.ratio(rm, rf), lwd = 2)
-   }
-abline(h = 1, lty = "55")
 
 
 ###################################################
--- Rsolutions/renal-s.R
+++ Rsolutions/renal-s.R
@@ -1,7 +1,7 @@
 ### R code from vignette source '/home/runner/work/SPE/SPE/build/renal-s.rnw'
 
 ###################################################
-### code chunk number 1: renal-s.rnw:3-7
+### code chunk number 1: renal-s.rnw:12-16
 ###################################################
 options( width=90,
          prompt=" ", continue=" ",
@@ -10,20 +10,21 @@ options( width=90,
 
 
 ###################################################
-### code chunk number 2: renal-s.rnw:19-27
+### code chunk number 2: renal-s.rnw:84-93
 ###################################################
-library( Epi ) 
+library( Epi )
 library( foreign )
 clear()
 renal <- read.dta(
-"https://raw.githubusercontent.com/SPE-R/SPE/master/pracs/data/renal.dta")
+         "https://raw.githubusercontent.com/SPE-R/SPE/master/pracs/data/renal.dta")
+# renal <- read.dta( "http://BendixCarstensen.com/SPE/data/renal.dta" )
 # renal <- read.dta( "./data/renal.dta" )
 renal$sex <- factor( renal$sex, labels=c("M","F") )
-head( renal )
+head(renal)
 
 
 ###################################################
-### code chunk number 3: renal-s.rnw:40-48
+### code chunk number 3: renal-s.rnw:127-135
 ###################################################
 Lr <- Lexis( entry = list( per = doe,
                            age = doe-dob,
@@ -52,9 +53,9 @@ plot( Lr, col="black", lwd=3 )
 
 
 ###################################################
-### code chunk number 6: renal-s.rnw:81-87
+### code chunk number 6: renal-s.rnw:194-200
 ###################################################
-pdf( "./graph/renal-Lexis-fancy.pdf", height=80/9+1, width=40/9+1 )
+pdf( "./graph/renal-Lexis-fancy.pdf", height=80/5+1, width=40/5+1 )
 par( mai=c(3,3,1,1)/4, mgp=c(3,1,0)/1.6 )
 plot( Lr, 1:2, col=c("blue","red")[Lr$sex], lwd=3, grid=0:20*5,
       xlab="Calendar time", ylab="Age",
@@ -63,7 +64,7 @@ dev.off()
 
 
 ###################################################
-### code chunk number 7: renal-s.rnw:108-112
+### code chunk number 7: renal-s.rnw:234-238
 ###################################################
 library( survival )
 mc <- coxph( Surv( lex.dur, lex.Xst=="ESRD" ) ~
@@ -72,83 +73,59 @@ summary( mc )
 
 
 ###################################################
-### code chunk number 8: renal-s.rnw:123-125
-###################################################
-mc <- coxph.Lexis( Lr, formula = tfi ~ I(age/10) + sex )
-summary( mc )
-
-
-###################################################
-### code chunk number 9: renal-s.rnw:140-146
+### code chunk number 8: renal-s.rnw:290-296
 ###################################################
 Lc <- cutLexis( Lr, cut = Lr$dor, # where to cut follow up
               timescale = "per",  # what timescale are we referring to
               new.state = "Rem",  # name of the new state
-            split.state = TRUE,   # state subdivided by origin of transition
+            split.state = TRUE,   # different states sepending on previous
        precursor.states = "NRA" ) # which states are less severe
 summary( Lc )
 
 
 ###################################################
-### code chunk number 10: renal-s.rnw:153-155
+### code chunk number 9: renal-s.rnw:303-305
 ###################################################
 subset( Lr, lex.id %in% c(2:4,21) )[,c(1:9,12)]
 subset( Lc, lex.id %in% c(2:4,21) )[,c(1:9,12)]
 
 
 ###################################################
-### code chunk number 11: Lc-boxes
+### code chunk number 10: Lc-boxes
 ###################################################
-boxes( Relevel(Lc,c(1,2,4,3)), boxpos=TRUE, scale.R=100, show.BE=TRUE )
+# boxes( Lc, boxpos=TRUE, scale.R=100, show.BE=TRUE, hm=1.5, wm=1.5 )
+boxes( Relevel(Lc,c(1,2,4,3)),
+       boxpos=TRUE, scale.R=100, show.BE=TRUE, hm=1.5, wm=1.5 )
 
 
 ###################################################
-### code chunk number 12: Lexis-rem
+### code chunk number 11: Lexis-rem
 ###################################################
 par( mai=c(3,3,1,1)/4, mgp=c(3,1,0)/1.6 )
-plot( Lc, col=c("red","forestgreen")[Lc$lex.Cst],
+plot( Lc, col=c("red","limegreen")[Lc$lex.Cst],
       xlab="Calendar time", ylab="Age",
       lwd=3, grid=0:20*5, xlim=c(1970,2010), ylim=c(0,80), xaxs="i", yaxs="i", las=1 )
 points( Lc, pch=c(NA,NA,16,16)[Lc$lex.Xst],
-            col=c("red","forestgreen","transparent")[Lc$lex.Cst])
+            col=c("red","limegreen","transparent")[Lc$lex.Cst])
 points( Lc, pch=c(NA,NA,1,1)[Lc$lex.Xst],
             col="black", lwd=2 )
 
 
 ###################################################
-### code chunk number 13: renal-s.rnw:202-212
+### code chunk number 12: renal-s.rnw:403-411
 ###################################################
 ( EP <- levels(Lc)[3:4] )
 m1 <- coxph( Surv( tfi,                  # from
                    tfi+lex.dur,          # to
                    lex.Xst %in% EP ) ~   # event
              sex + I((doe-dob-50)/10) +  # fixed covariates
-             (lex.Cst=="Rem"),           # time-dependent variable 
+             (lex.Cst=="Rem"),           # time-dependent variable
              data = Lc )
-m1 <- coxph.Lexis( Lc, formula= tfi ~ sex + I((doe-dob-50)/10) +
-                                      (lex.Cst=="Rem") )     
 summary( m1 )
 
 
 ###################################################
-### code chunk number 14: renal-s.rnw:227-233
-###################################################
-mt <- coxph.Lexis( Lc, formula = 
-                       tfi ~ sex + I((doe-dob-50)/10) + (lex.Cst=="Rem") )
-ma <- coxph.Lexis( Lc, formula = 
-                       age ~ sex + I(tfi/10) + (lex.Cst=="Rem") )
-cbind( ci.exp(m1), ci.exp(mt) )
-ci.exp( ma )
-
-
-###################################################
-### code chunk number 15: renal-s.rnw:242-243
-###################################################
-cox.zph( m1 )
-
-
-###################################################
-### code chunk number 16: renal-s.rnw:259-262
+### code chunk number 13: renal-s.rnw:446-449
 ###################################################
 sLc <- splitLexis( Lc, "tfi", breaks=seq(0,30,1/12) )
 summary( Lc, scale=100 )
@@ -156,45 +133,29 @@ summary(sLc, scale=100 )
 
 
 ###################################################
-### code chunk number 17: renal-s.rnw:267-270
-###################################################
-library( popEpi )
-sLc <- splitMulti( Lc, tfi=seq(0,30,1/12) )
-summary(sLc, scale=100 )
-
-
-###################################################
-### code chunk number 18: renal-s.rnw:281-287
+### code chunk number 14: renal-s.rnw:476-483
 ###################################################
 library( splines )
-mp <- glm( cbind(lex.Xst %in% EP,lex.dur) ~ Ns( tfi, knots=c(0,2,5,9,14) ) +
-                            sex + I((doe-dob-40)/10) + I(lex.Cst=="Rem"),
-           family = poisreg, 
+mp <- glm( lex.Xst %in% EP ~ Ns( tfi, knots=c(0,2,5,10) ) +
+           sex + I((doe-dob-40)/10) + I(lex.Cst=="Rem"),
+           offset = log(lex.dur),
+           family = poisson,
              data = sLc )
 ci.exp( mp )
 
 
 ###################################################
-### code chunk number 19: renal-s.rnw:293-296
-###################################################
-mpx <- glm.Lexis( sLc, formula=~ Ns( tfi, knots=c(0,2,5,10) ) +
-                                 sex + I((doe-dob-40)/10) + I(lex.Cst=="Rem") )
-ci.exp( mpx )
-
-
-###################################################
-### code chunk number 20: renal-s.rnw:304-310
+### code chunk number 15: renal-s.rnw:503-508
 ###################################################
 library( mgcv )
-mx <- gam.Lexis( sLc, formula=~ s( tfi, k=10 ) +
-                                sex + I((doe-dob-40)/10) + I(lex.Cst=="Rem") )
-ci.exp( mx )
-round( ci.exp( mp, subset=c("Cst","doe","sex") ), 3 )
-round( ci.exp( mx, subset=c("Cst","doe","sex") ), 3 )
+mx <- gam.Lexis(sLc, 
+                ~ s(tfi, k=10) + sex + I((doe-dob-40)/10) + I(lex.Cst=="Rem"))
+ci.exp( mp, subset=c("Cst","doe","sex") )
+ci.exp( mx, subset=c("Cst","doe","sex") )
 
 
 ###################################################
-### code chunk number 21: renal-s.rnw:317-320
+### code chunk number 16: renal-s.rnw:524-527
 ###################################################
 ci.exp( mx, subset=c("sex","dob","Cst"), pval=TRUE )
 ci.exp( m1 )
@@ -202,96 +163,78 @@ round( ci.exp( mp, subset=c("sex","dob","Cst") ) / ci.exp( m1 ), 3 )
 
 
 ###################################################
-### code chunk number 22: tfi-gam
+### code chunk number 17: tfi-gam
 ###################################################
 plot( mx )
 
 
 ###################################################
-### code chunk number 23: pred
+### code chunk number 18: pred
 ###################################################
 nd <- data.frame( tfi = seq(0,20,.1),
                   sex = "M",
                   doe = 1990,
                   dob = 1940,
-              lex.Cst = "NRA" )
+              lex.Cst = "NRA",
+              lex.dur = 100 )
 str( nd )
 matshade( nd$tfi, cbind( ci.pred( mp, newdata=nd ),
-                         ci.pred( mx, newdata=nd ) )*100, plot=TRUE,
+                         ci.pred( mx, newdata=nd ) ), plot=TRUE,
           type="l", lwd=3:4, col=c("black","forestgreen"),
           log="y", xlab="Time since entry (years)",
-                   ylab="ESRD rate (per 100 PY) for 50 year old man" )
-rug(c(0,2,5,9,14))
+                   ylab="ESRD rate (per 100 PY) for 50 year man" )
 
 
 ###################################################
-### code chunk number 24: renal-s.rnw:385-386
+### code chunk number 19: rem-inc-mgcv
 ###################################################
-sLc <- transform( sLc, tfr = pmax( (per-dor)/10, 0, na.rm=TRUE ) )
+mr <- gam.Lexis(sLc, ~ s( tfi, k=10 ) + sex,
+                     from = "NRA", 
+                       to = "Rem")
+ci.exp(mr, pval = TRUE)
 
 
 ###################################################
-### code chunk number 25: time-since-rem
+### code chunk number 20: renal-s.rnw:732-749
 ###################################################
-mPx <- gam.Lexis( sLc, formula= ~ s( tfi, k=10 ) +
-                       factor(sex) + I((doe-dob-40)/10) +  
-                       I(lex.Cst=="Rem") + tfr )
-round( ci.exp( mPx ), 3 )
-
-
-###################################################
-### code chunk number 26: rem-inc-mgcv
-###################################################
-mr <- gam( cbind(lex.Xst=="Rem",lex.dur) ~ s( tfi, k=10 ) + sex,
-           family = poisreg,
-             data = subset( sLc, lex.Cst=="NRA" ) )
-
-
-###################################################
-### code chunk number 27: renal-s.rnw:431-433
-###################################################
-mr <- gam.Lexis( sLc, to="Rem", formula= ~ s( tfi, k=10 ) + sex )
-ci.exp( mr, pval=TRUE )
-
-
-###################################################
-### code chunk number 28: renal-s.rnw:450-466
-###################################################
-inL <- sLc[1,1:11]
-timeSince(inL)
+inL <- subset( sLc, select=1:11 )[NULL,]
+str( inL )
+timeScales(inL)
 inL[1,"lex.id"] <- 1
 inL[1,"per"] <- 2000
 inL[1,"age"] <- 50
 inL[1,"tfi"] <- 0
-inL[1,"lex.dur"] <- NA
 inL[1,"lex.Cst"] <- "NRA"
 inL[1,"lex.Xst"] <- NA
+inL[1,"lex.dur"] <- NA
 inL[1,"sex"] <- "M"
 inL[1,"doe"] <- 2000
 inL[1,"dob"] <- 1950
-inL <- inL[c(1,1),]
+inL <- rbind( inL, inL )
 inL[2,"sex"] <- "F"
 inL
 str( inL )
 
 
 ###################################################
-### code chunk number 29: renal-s.rnw:475-478
+### code chunk number 21: renal-s.rnw:758-761
 ###################################################
-Tr <- list( "NRA" = list( "Rem"  = mr,
-                          "ESRD" = mx ),
-            "Rem" = list( "ESRD(Rem)" = mx ) )
+Tr <- list("NRA" = list("Rem"       = mr,
+                        "ESRD"      = mx ),
+           "Rem" = list("ESRD(Rem)" = mx))
 
 
 ###################################################
-### code chunk number 30: 10000-sim
+### code chunk number 22: 10000-sim
 ###################################################
 system.time( sM <- simLexis( Tr, inL, N=5000 ) )
+# save( sM, file="sM.Rda" )
+# load(     file="sM.Rda" )
 summary( sM, by="sex" )
 
 
 ###################################################
-### code chunk number 31: nState
+### code chunk number 23: nState
 ###################################################
 nStm <- nState( subset(sM,sex=="M"), at=seq(0,10,0.1), from=50, time.scale="age" )
 nStf <- nState( subset(sM,sex=="F"), at=seq(0,10,0.1), from=50, time.scale="age" )
@@ -299,39 +242,37 @@ head( nStf )
 
 
 ###################################################
-### code chunk number 32: pState
+### code chunk number 24: pState
 ###################################################
-prm <- c(2,1,3,4)
-colnames( nStm )[prm]
-ppm <- pState( nStm, perm=prm )
-ppf <- pState( nStf, perm=prm )
+ppm <- pState( nStm, perm=c(2,1,3,4) )
+ppf <- pState( nStf, perm=c(2,1,3,4) )
 head( ppf )
 tail( ppf )
 
 
 ###################################################
-### code chunk number 33: plot-pp
+### code chunk number 25: plot-pp
 ###################################################
 plot( ppf )
 
 
 ###################################################
-### code chunk number 34: new-pState
+### code chunk number 26: new-pState
 ###################################################
-clr <- c("forestgreen","orange")
-clr <- c(clr,adjustcolor(rev(clr),alpha=0.7))
 par( mfrow=c(1,2), mar=c(3,1.5,1,1), oma=c(0,2,0,0), las=1 )
-plot( ppm, col=clr )
-lines( as.numeric(rownames(ppm)), ppm[,"NRA"], lwd=4 )
+#
+plot( ppm, col=c("limegreen","red","#991111","forestgreen") )
 mtext( "Probability", side=2, las=0, outer=T, line=0.5 )
-mtext( "Men", side=3 )
+lines( as.numeric(rownames(ppm)), ppm[,"NRA"], lwd=4 )
+text( 59.5, 0.95, "Men", adj=1, col="white", font=2, cex=1.2 )
 axis( side=4, at=0:10/10 )
 axis( side=4, at=0:20/20 , labels=NA, tck=-0.02 )
 axis( side=4, at=1:99/100, labels=NA, tck=-0.01 )
-
-plot( ppf, col=clr, xlim=c(60,50), yaxt="n", ylab="" )
+#
+plot( ppf, col=c("limegreen","red","#991111","forestgreen"),
+           xlim=c(60,50), yaxt="n", ylab="" )
 lines( as.numeric(rownames(ppf)), ppf[,"NRA"], lwd=4 )
-mtext( "Women", side=3 )
+text( 59.5, 0.95, "Women", adj=0, col="white", font=2, cex=1.2 )
 axis( side=2, at=0:10/10 , labels=NA )
 axis( side=2, at=0:20/20 , labels=NA, tck=-0.02 )
 axis( side=2, at=1:99/100, labels=NA, tck=-0.01 )
