1b12b099d11d50ad29a850571aa6b6c62677c924
Damien Georges <damien.georges2@gmail.com>
2024-05-06 19:22:51 +0200

    - format R code with styler - ensure the code chunck do not exceed 66pts - use custom header in the pdf file

--- pracs-book/data/alkfos-house.r
+++ pracs-book/data/alkfos-house.r
@@ -1,22 +1,21 @@
-alkfos.pctchange<-(sweep(alkfos[-1],1,alkfos$c0,"/")-1)*100
-available<-aggregate(!is.na(alkfos[-1]),list(alkfos$grp),sum)
-means<-aggregate(alkfos.pctchange,list(alkfos$grp),mean,na.rm=T)
-sds<-aggregate(alkfos.pctchange,list(alkfos$grp),sd,na.rm=T)
-available<-as.matrix(available[-1])
-means<-as.matrix(means[-1])
-sds<-as.matrix(sds[-1])
+alkfos.pctchange <- (sweep(alkfos[-1], 1, alkfos$c0, "/") - 1) * 100
+available <- aggregate(!is.na(alkfos[-1]), list(alkfos$grp), sum)
+means <- aggregate(alkfos.pctchange, list(alkfos$grp), mean, na.rm = TRUE)
+sds <- aggregate(alkfos.pctchange, list(alkfos$grp), sd, na.rm = TRUE)
+available <- as.matrix(available[-1])
+means <- as.matrix(means[-1])
+sds <- as.matrix(sds[-1])
 
-sems<-sds/sqrt(available)
+sems <- sds / sqrt(available)
 
-times<-c(0,3,6,9,12,18,24)
+times <- c(0, 3, 6, 9, 12, 18, 24)
 
 ### Put data in long format in a data frame for ggplot2
 ggdata <- data.frame(
-    times = rep(times, 2),
-    means = c(means[1,], means[2,]),
-    sds = c(sds[1,], sds[2,]),
-    available = c(available[1,], available[2,]),
-    treat = rep(c("placebo","tamoxifen"), each=7)
-    )
-ggdata <- transform(ggdata, sems = sds/sqrt(available))
-
+  times = rep(times, 2),
+  means = c(means[1, ], means[2, ]),
+  sds = c(sds[1, ], sds[2, ]),
+  available = c(available[1, ], available[2, ]),
+  treat = rep(c("placebo", "tamoxifen"), each = 7)
+)
+ggdata <- transform(ggdata, sems = sds / sqrt(available))
