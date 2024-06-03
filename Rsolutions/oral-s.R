9a39bced45c1cc6beebf8b5b3a0cf75232194c4d
janpitka <32964576+janpitka@users.noreply.github.com>
2024-06-03 07:33:51 +0000

    Update gh-spe-material to output generated at cc83611

--- Rsolutions/oral-s.R
+++ Rsolutions/oral-s.R
@@ -2,270 +2,279 @@
 knitr::opts_chunk$set(keep.source = TRUE, eps = FALSE, include = TRUE, prefix.string = "./graph/oral")
 
 
-## ----packages, echo=T,eval=FALSE----------------------------------------------
-## library(Epi)
-## library(survival)
-## cB8  <- c("#000000", "#E69F00", "#56B4E9", "#009E73",
-##           "#F0E442", "#0072B2", "#D55E00", "#CC79A7") #colors chosen
-## options(digits=3)
+## ----packages, echo=T,eval=TRUE-----------------------------------------------
+library(Epi)
+library(survival)
+cB8  <- c("#000000", "#E69F00", "#56B4E9", "#009E73", 
+          "#F0E442", "#0072B2", "#D55E00", "#CC79A7") #colors chosen
+options(digits=3)
 
 
-## ----dinput, echo=T,eval=FALSE------------------------------------------------
-## orca <- read.table("data/oralca2.txt", header = TRUE)
-## head(orca)
-## str(orca)
-## summary(orca)
+## ----dinput, echo=T,eval=TRUE-------------------------------------------------
+orca <-  read.table(file = 'https://raw.githubusercontent.com/SPE-R/SPE/master/pracs/data/oralca2.txt', header = TRUE, sep = " ",row.names = 1 )
+head(orca)
+orca$stage<-as.factor(orca$stage)
+orca$sex<-factor(orca$sex,levels=c("Male","Female"))
+str(orca)
+summary(orca)
 
 
-## ----Survobject, echo=T,eval=FALSE--------------------------------------------
-## orca$suob <- Surv(orca$time, 1 * (orca$event > 0))
-## str(orca$suob)
-## summary(orca$suob)
+## ----Survobject, echo=T,eval=TRUE---------------------------------------------
+suob <- Surv(orca$time, 1 * (orca$event > 0))
+str(orca$suob)
+summary(orca$suob)
 
 
-## ----surv1, echo=T,eval=FALSE-------------------------------------------------
-## s.all <- survfit(suob ~ 1, data = orca)
+## ----surv1, echo=T,eval=TRUE--------------------------------------------------
+s.all <- survfit(suob ~ 1, data = orca)
 
 
-## ----surv2, echo=T,eval=FALSE-------------------------------------------------
-## s.all
-## str(s.all)
+## ----surv2, echo=T,eval=TRUE--------------------------------------------------
+s.all
+str(s.all)
 
 
-## ----survcdf1, echo=TRUE, fig=TRUE, height=6, width=7,eval=FALSE--------------
-## plot(s.all,main="KM estimate of the survival
-##      and cum. mortality proportions",
-##      xlab="years", ylab="Survival")
-## lines(s.all, fun = "event", mark.time = F, conf.int = FALSE)
+## ----survcdf1, echo=TRUE, fig=TRUE, height=6, width=7,eval=TRUE---------------
+plot(s.all,main="KM estimate of the survival
+     and cum. mortality proportions",
+     xlab="years", ylab="Survival")
+plot(s.all)
+lines(s.all, fun = "event", mark.time = F, conf.int = FALSE)
 
 
-## ----cdfstage, echo=T, fig=T, height=6, width=7,eval=FALSE--------------------
-## s.stg <- survfit(suob ~ stage, data = orca)
-## col5 <- c("green", "blue", "black", "red", "gray")
-## plot(s.stg, col = col5, fun = "event", mark.time = FALSE)
-## s.stg
+## ----cdfstage, echo=T, fig=T, height=6, width=7,eval=TRUE---------------------
+s.stg <- survfit(suob ~ stage, data = orca)
+col5 <- cB8[1:5]
+plot(s.stg, col = col5, fun = "event", main="Cum. mortality by stage",mark.time = FALSE)
+legend(15, 0.6, title="stage",legend=levels(orca$stage),col = col5,lty=rep(1,5))
 
 
-## ----chstage, echo=T, fig=T, height=6, width=12,eval=FALSE--------------------
-## par(mfrow = c(1, 2))
-## plot(s.stg, col = col5, fun = "cumhaz", main = "cum. hazards")
-## plot(
-##   s.stg,
-##   col = col5,
-##   fun = "cloglog",
-##   main = "cloglog: log cum.haz"
-## )
+## ----chstage, echo=T, fig=T, height=6, width=12,eval=TRUE---------------------
+par(mfrow = c(1, 2))
+plot(s.stg, col = col5, fun = "cumhaz", main = "Cumulative hazards")
+legend(1, 3.5, title="stage",legend=levels(orca$stage),col = col5,lty=rep(1,5), cex=0.8)
+plot(s.stg, col = col5, fun = "cloglog",main = "cloglog: log cum.haz")
+legend(3, -2, title="stage",legend=levels(orca$stage),col = col5,lty=rep(1,5), cex=0.7)
 
 
-## ----sexage, echo=T,eval=FALSE------------------------------------------------
-## orca$agegr <- cut(orca$age, br = c(0, 55, 75, 95))
-## stat.table(list(sex, agegr), list(count(), percent(agegr)),
-##   margins = TRUE,
-##   data = orca
-## )
+
+## ----sexage, echo=T,eval=TRUE-------------------------------------------------
+orca$agegr <- cut(orca$age, br = c(0, 55, 75, 95))
+stat.table(list(sex, agegr), list(count(), percent(agegr)),
+  margins = TRUE, 
+  data = orca
+)
 
 
-## ----cdfsexage, echo=TRUE, fig=TRUE, height=6, width=7,eval=FALSE-------------
-## s.agrx <- survfit(suob ~ agegr + sex, data=orca)
-## par(mfrow=c(1,1))
-## plot(s.agrx, fun="event", mark.time=F, xlim = c(0,15), lwd=2,
-##              col=rep(c(cB8[7], cB8[6]),3), lty=c(2,2, 1,1, 5,5), pch=c(1,1,2,2,4,4))
-## legend(10,0.3, legend=c("(0,55] Female "," (0,55] Male",
-##                        "(55,75] Female "," (55,75] Male",
-##                        "(75,95] Female "," (75,95] Male" ),
-##        col=rep(c(cB8[7], cB8[6]),3), lty=c(2,2, 1,1, 5,5),
-##        pch=c(1,1,2,2,4,4),cex=0.65)
+## ----cdfsexage, echo=TRUE, fig=TRUE, height=6, width=7,eval=TRUE--------------
+s.agrx <- survfit(suob ~ agegr + sex, data=orca)
+par(mfrow=c(1,1))
+plot(s.agrx, fun="event", main="Cumulative mortality (KM) by age and sex",xlab="Time since oral cancer diagnosis (years)",ylab="Cum. mortality",mark.time=F, xlim = c(0,15), lwd=2,
+             col=rep(c(cB8[7], cB8[6]),3), lty=c(2,2, 1,1, 5,5), pch=c(1,1,2,2,4,4), xaxs="i",yaxs="i")
+legend(12,0.35, legend=c("(0,55] Female "," (0,55] Male",
+                       "(55,75] Female "," (55,75] Male",
+                       "(75,95] Female "," (75,95] Male" ),
+       col=rep(c(cB8[7], cB8[6]),3), lty=c(2,2, 1,1, 5,5),
+       pch=c(1,1,2,2,4,4),cex=0.65)
 
 
-## ----cif1a, echo=T,eval=FALSE-------------------------------------------------
-## library(survival)
-## cif1 <- survfit(Surv(time, event, type = "mstate") ~ 1,
-##   data = orca
-## )
-## str(cif1)
+## ----cif1a, echo=T,eval=TRUE--------------------------------------------------
+library(survival)
+cif1 <- survfit(Surv(time, event, type = "mstate") ~ 1,
+  data = orca
+)
+str(cif1)
 
 
-## ----plotcif1, echo=T,fig=T,eval=FALSE----------------------------------------
-## par(mfrow = c(1, 2))
-## plotCIF(cif1, 1, main = "Cancer death")
-## plotCIF(cif1, 2, main = "Other deaths")
+## ----plotcif1, echo=T,fig=T,eval=TRUE-----------------------------------------
+par(mfrow = c(1, 2))
+plotCIF(cif1, 1, main = "Cancer death",xlab="Time since oral cancer diagnosis (years)")
+plotCIF(cif1, 2, main = "Other deaths",xlab="Time since oral cancer diagnosis (years)")
 
 
-## ----plotcif2, echo=T, fig=T, height=6, width=10,eval=FALSE-------------------
-## col5 <- c("green", "blue", "black", "red", "gray")
-## cif2 <- survfit(Surv(time, event, type = "mstate") ~ stage,
-##   data = orca
-## )
-## str(cif2)
-## 
-## par(mfrow = c(1, 2))
-## plotCIF(cif2, 1,
-##   main = "Cancer death by stage",
-##   col = col5, ylim = c(0, 0.7)
-## )
-## plotCIF(cif2, 2,
-##   main = "Other deaths by stage",
-##   col = col5, ylim = c(0, 0.7)
-## )
+## ----plotcif2, echo=T, fig=T, height=6, width=10,eval=TRUE--------------------
+col5 <- col5
+cif2 <- survfit(Surv(time, event, type = "mstate") ~ stage,
+  data = orca
+)
+str(cif2)
+
+par(mfrow = c(1, 2))
+plotCIF(cif2, 1,
+  main = "Cancer death by stage",
+  col = col5, ylim = c(0, 0.7)
+)
+plotCIF(cif2, 2,
+  main = "Other deaths by stage",
+  col = col5, ylim = c(0, 0.7)
+)
+
+legend(3, 0.7, title="stage",legend=levels(orca$stage),col = col5,lty=rep(1,5), cex=0.7)
 
 
-## ----stackedcif1, echo=TRUE, fig=TRUE, eval=FALSE-----------------------------
-## par(mfrow=c(1,1),xaxs="i", yaxs="i") # make plot start 0,0
-## stackedCIF(cif1,xlim=c(0,20),
-##            col = c("black"),
-##            fill=c(cB8[6],cB8[8],cB8[2]))
-## text( 10, 0.10, "Oral ca death ", pos = 4)
-## text( 10, 0.5, " Other death ", pos = 4)
-## text( 10, 0.80, " Alive ", pos = 4)
+
+## ----stackedcif1, echo=TRUE, fig=TRUE, eval=TRUE------------------------------
+par(mfrow=c(1,1),xaxs="i", yaxs="i") # make plot start 0,0
+stackedCIF(cif1,xlim=c(0,20),
+           col = c("black"),
+           fill=c(cB8[6],cB8[8],cB8[2])) #choosing some colors 
+text( 10, 0.10, "Oral ca death ", pos = 4)
+text( 10, 0.5, " Other death ", pos = 4)
+text( 10, 0.80, " Alive ", pos = 4)
 
 
-## ----cox1, echo=T,eval=FALSE--------------------------------------------------
-## options(show.signif.stars = FALSE)
-## m1 <- coxph(suob ~ sex + I((age - 65) / 10) + stage, data = orca)
-## summary(m1)
-## round(ci.exp(m1), 4)
+## ----cox1, echo=T,eval=TRUE---------------------------------------------------
+options(show.signif.stars = FALSE)
+m1 <- coxph(suob ~ sex + I((age - 65) / 10) + stage, data = orca)
+summary(m1)
+round(ci.exp(m1),3)
 
 
-## ----coxzph, echo=T,eval=FALSE------------------------------------------------
-## cox.zph(m1)
+## ----coxzph, echo=T,eval=TRUE-------------------------------------------------
+cox.zph(m1)
 
 
-## ----cox2, echo=T,eval=FALSE--------------------------------------------------
-## orca2 <- subset(orca, stage != "unkn")
-## orca2$st3 <- Relevel(orca2$stage, list(1:2, 3, 4:5))
-## levels(orca2$st3) <- c("I-II", "III", "IV")
-## m2 <- update(m1, . ~ . - stage + st3, data = orca2)
-## round(ci.exp(m2), 4)
+## ----cox2, echo=T,eval=TRUE---------------------------------------------------
+orca2 <- subset(orca, stage != "unkn")
+orca2$st3 <- Relevel(orca2$stage, list(1:2, 3, 4:5))
+levels(orca2$st3) <- c("I-II", "III", "IV")
+m2 <- coxph(Surv(orca2$time, 1 * (orca2$event > 0)) ~ sex + I((age - 65) / 10) + st3, data = orca2)
+summary(m2)
+#m2 <- update(m1, . ~ . - stage + st3, data = orca2) #do not work
+round(ci.exp(m2), 3)
 
 
-## ----cox2cll, echo=T,eval=FALSE-----------------------------------------------
-## newd <- data.frame(
-##   sex = c(rep("Male", 6), rep("Female", 6)),
-##   age = rep(c(rep(40, 3), rep(80, 3)), 2),
-##   st3 = rep(levels(orca2$st3), 4)
-## )
-## newd
-## col3 <- c("green", "black", "red")
-## par(mfrow = c(1, 2))
-## plot(
-##   survfit(
-##     m2, newdata = subset(newd, sex == "Male" & age == 40)
-##   ),
-##   col = col3, fun = "event", mark.time = FALSE
-## )
-## lines(
-##   survfit(
-##     m2, newdata = subset(newd, sex == "Female" & age == 40)
-##   ),
-##   col = col3, fun = "event", lty = 2, mark.time = FALSE
-## )
-## plot(
-##   survfit(
-##     m2, newdata = subset(newd, sex == "Male" & age == 80)
-##   ),
-##   ylim = c(0, 1), col = col3, fun = "event", mark.time = FALSE
-## )
-## lines(
-##   survfit(
-##     m2, newdata = subset(newd, sex == "Female" & age == 80)
-##   ),
-##   col = col3, fun = "event", lty = 2, mark.time = FALSE
-## )
+## ----cox2cll, echo=T,eval=TRUE------------------------------------------------
+newd <- data.frame(
+  sex = c(rep("Male", 6), rep("Female", 6)),
+  age = rep(c(rep(40, 3), rep(80, 3)), 2),
+  st3 = rep(levels(orca2$st3), 4)
+)
+newd
+col3 <- cB8[1:3] #pre-setting color palette
+leg<-levels(interaction(levels(factor(orca2$sex)),levels(orca2$st3))) #legend labels by sex and stage
+par(mfrow = c(1, 2))
+plot(
+  survfit(m2, newdata = subset(newd, sex == "Male" & age == 40)),
+  col = col3, 
+  lty= 1,
+  fun = "event", mark.time = FALSE,
+  main="Cum. mortality for M and F \n age 40"
+  )
+lines(
+  survfit(m2, newdata = subset(newd, sex == "Female" & age == 40)),
+  col = col3, lty=c(2,2,2),fun = "event", mark.time = FALSE
+)
+legend(0, 0.95, title="stage",legend=leg,
+       col = c(col3[1],col3[1],col3[2],col3[2],col3[3],col3[3]),
+        ,lty=c(2,1,2,1,2,1), cex=0.6)
+plot(
+  survfit(m2, newdata = subset(newd, sex == "Male" & age == 80)),
+  ylim = c(0, 1), col = col3, fun = "event", mark.time = FALSE,
+   main="Cum. mortality for M and F \n age 80"
+)
+lines(
+  survfit(m2, newdata = subset(newd, sex == "Female" & age == 80)  ),
+  col = col3, fun = "event", lty = 2, mark.time = FALSE
+)
+legend(10, 0.5, title="stage",legend=leg,
+       col = c(col3[1],col3[1],col3[2],col3[2],col3[3],col3[3]),
+        ,lty=c(2,1,2,1,2,1), cex=0.7)
 
 
-## ----coxhaz1, echo=T,eval=FALSE-----------------------------------------------
-## m2haz1 <-
-##   coxph(
-##     Surv(time, event == 1) ~ sex + I((age - 65) / 10) + st3,
-##     data = orca2
-##   )
-## round(ci.exp(m2haz1), 4)
-## cox.zph(m2haz1)
+## ----coxhaz1, echo=T,eval=TRUE------------------------------------------------
+m2haz1 <- 
+  coxph(
+    Surv(time, event == 1) ~ sex + I((age - 65) / 10) + st3, 
+    data = orca2
+  )
+round(ci.exp(m2haz1), 4)
+cox.zph(m2haz1)
 
 
-## ----coxhaz2, echo=T,eval=FALSE-----------------------------------------------
-## m2haz2 <-
-##   coxph(
-##     Surv(time, event == 2) ~ sex + I((age - 65) / 10) + st3,
-##     data = orca2
-##   )
-## round(ci.exp(m2haz2), 4)
-## cox.zph(m2haz2)
+## ----coxhaz2, echo=T,eval=TRUE------------------------------------------------
+m2haz2 <- 
+  coxph(
+    Surv(time, event == 2) ~ sex + I((age - 65) / 10) + st3, 
+    data = orca2
+  )
+round(ci.exp(m2haz2), 4)
+cox.zph(m2haz2)
 
 
-## ----lexis 1, echo=T,eval=FALSE-----------------------------------------------
-## orca.lex <- Lexis(
-##   exit = list(stime = time),
-##   exit.status = factor(event,
-##     labels = c("Alive", "Oral ca. death", "Other death")
-##   ),
-##   data = orca
-## )
-## summary(orca.lex)
+## ----lexis 1, echo=T,eval=TRUE------------------------------------------------
+orca.lex <- Lexis(
+  exit = list(stime = time),
+  exit.status = factor(event,
+    labels = c("Alive", "Oral ca. death", "Other death")
+  ),
+  data = orca
+)
+summary(orca.lex)
 
 
-## ----lexis, echo=T,eval=FALSE-------------------------------------------------
-## boxes(orca.lex)
+## ----lexis, echo=T,eval=TRUE--------------------------------------------------
+boxes(orca.lex,boxpos = TRUE)
 
 
-## ----split, echo=T,eval=FALSE-------------------------------------------------
-## orca2.lex <- subset(orca.lex, stage != "unkn")
-## orca2.lex$st3 <- Relevel(orca2$stage, list(1:2, 3, 4:5))
-## levels(orca2.lex$st3) <- c("I-II", "III", "IV")
+## ----split, echo=T,eval=TRUE--------------------------------------------------
+orca2.lex <- subset(orca.lex, stage != "unkn")
+orca2.lex$st3 <- Relevel(orca2.lex$stage, list(1:2, 3, 4:5))
+levels(orca2.lex$st3) <- c("I-II", "III", "IV")
 
 
-## ----split b, echo=T,eval=FALSE-----------------------------------------------
-## cuts <- sort(orca2$time[orca2$event == 1])
-## orca2.spl <-
-##   splitLexis(orca2.lex, br = cuts, time.scale = "stime")
-## orca2.spl$timeband <- as.factor(orca2.spl$stime)
+## ----split b, echo=T,eval=TRUE------------------------------------------------
+cuts <- sort(orca2.lex$time[orca2.lex$event == 1])
+orca2.spl <- 
+  splitLexis(orca2.lex, br = cuts, time.scale = "stime")
+orca2.spl$timeband <- as.factor(orca2.spl$stime)
 
 
-## ----strsplit, echo=T,eval=FALSE----------------------------------------------
-## str(orca2.spl)
-## orca2.spl[1:20, ]
+## ----strsplit, echo=T,eval=TRUE-----------------------------------------------
+str(orca2.spl)
+head(orca2.spl)
 
 
-## ----poisson, echo=T,eval=FALSE-----------------------------------------------
-## m2pois1 <- glm(
-##   1 * (lex.Xst == "Oral ca. death") ~
-##     -1 + timeband + sex + I((age - 65) / 10) + st3,
-##   family = poisson, offset = log(lex.dur), data = orca2.spl
-## )
+## ----poisson, echo=T,eval=TRUE------------------------------------------------
+m2pois1 <- glm(
+  1 * (lex.Xst == "Oral ca. death") ~
+    -1 + timeband + sex + I((age - 65) / 10) + st3,
+  family = poisson, offset = log(lex.dur), data = orca2.spl
+)
 
 
-## ----poissonresults, echo=T, fig=T,eval=FALSE---------------------------------
-## tb <- as.numeric(levels(orca2.spl$timeband))
-## ntb <- length(tb)
-## tbmid <- (tb[-ntb] + tb[-1]) / 2 # midpoints of the intervals
-## round(ci.exp(m2pois1), 3)
-## par(mfrow = c(1, 1))
-## plot(tbmid, 1000 * exp(coef(m2pois1)[1:(ntb - 1)]),
-##   ylim = c(5, 3000), log = "xy", type = "l"
-## )
+## ----poissonresults, echo=T, fig=T,eval=TRUE----------------------------------
+tb <- as.numeric(levels(orca2.spl$timeband))
+ntb <- length(tb)
+tbmid <- (tb[-ntb] + tb[-1]) / 2 # midpoints of the intervals
+round(ci.exp(m2pois1), 3)
+par(mfrow = c(1, 1))
+plot(tbmid, 1000 * exp(coef(m2pois1)[1:(ntb - 1)]),
+  ylim = c(5, 3000), log = "xy", type = "l"
+)
 
 
-## ----poissonspline, echo=T, fig=T,eval=FALSE----------------------------------
-## library(splines)
-## m2pspli <-
-##   update(
-##     m2pois1,
-##     . ~ ns(stime, df = 6, intercept = FALSE) +
-##       sex + I((age - 65) / 10) + st3)
-## round(ci.exp(m2pspli), 3)
-## news <- data.frame(
-##   stime = seq(0, 25, length = 301),
-##   lex.dur = 1000,
-##   sex = "Female",
-##   age = 65,
-##   st3 = "I-II"
-## )
-## blhaz <-
-##   predict(m2pspli, newdata = news, se.fit = TRUE, type = "link")
-## blh95 <- cbind(blhaz$fit, blhaz$se.fit) %*% ci.mat()
-## par(mfrow = c(1, 1))
-## matplot(news$stime, exp(blh95),
-##   type = "l", lty = c(1, 1, 1), lwd = c(2, 1, 1),
-##   col = rep("black", 3), log = "xy", ylim = c(5, 3000)
-## )
+## ----poissonspline, echo=T, fig=T,eval=TRUE-----------------------------------
+library(splines)
+m2pspli <- 
+  update(
+    m2pois1, 
+    . ~ ns(stime, df = 6, intercept = FALSE) +
+      sex + I((age - 65) / 10) + st3)
+round(ci.exp(m2pspli), 3)
+news <- data.frame(
+  stime = seq(0, 25, length = 301), 
+  lex.dur = 1000, 
+  sex = "Female",
+  age = 65, 
+  st3 = "I-II"
+)
+blhaz <- 
+  predict(m2pspli, newdata = news, se.fit = TRUE, type = "link")
+blh95 <- cbind(blhaz$fit, blhaz$se.fit) %*% ci.mat()
+par(mfrow = c(1, 1))
+matplot(news$stime, exp(blh95),
+  type = "l", lty = c(1, 1, 1), lwd = c(2, 1, 1),
+  col = rep("black", 3), log = "xy", ylim = c(5, 3000)
+)
 
--- prerequest.knit.md
+++ prerequest.knit.md
@@ -44,6 +44,7 @@ During the practical session, the following list of `R`the packages will be requ
 - renv
 - rmarkdown
 - sem
+- splines
 - stdReg
 - SuperLearner
 - survival
@@ -53,7 +54,7 @@ During the practical session, the following list of `R`the packages will be requ
 
 which can be installed with the following command:
 
-install.packages(c('bookdown', 'cowplot', 'cranly', 'dagitty', 'Epi', 'foreign', 'ggplot2', 'grid', 'knitr', 'mgcv', 'popEpi', 'PSweight', 'purrr', 'renv', 'rmarkdown', 'sem', 'stdReg', 'SuperLearner', 'survival', 'tidyverse', 'tmle', 'tools'))
+install.packages(c('bookdown', 'cowplot', 'cranly', 'dagitty', 'Epi', 'foreign', 'ggplot2', 'grid', 'knitr', 'mgcv', 'popEpi', 'PSweight', 'purrr', 'renv', 'rmarkdown', 'sem', 'splines', 'stdReg', 'SuperLearner', 'survival', 'tidyverse', 'tmle', 'tools'))
 
 ---
 This page have been automatically generated. Do not modify
