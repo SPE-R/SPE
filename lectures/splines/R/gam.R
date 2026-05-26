8290467ac002bb08e196b4b52ede6794043f270a
martynplummer <martyn.plummer@gmail.com>
2026-05-26 17:04:18 +0100

    Further updates to splines lecture

--- lectures/splines/R/gam.R
+++ lectures/splines/R/gam.R
@@ -1,6 +1,7 @@
 library(mgcv)
 set.seed(2) ## simulate some data... 
 dat <- gamSim(1,n=100,dist="normal",scale=2)
+dat <- dat[order(dat$x1),]
 
 b <- gam(y~s(x1), data=dat)
 summary(b)
@@ -10,8 +11,9 @@ title("Some simulated data")
 dev.off()
 
 png("gam-7.png")
-plot(b,pages=1,residuals=TRUE, pch=16, cex=0.5)  ## show partial residuals
-title("A gam fit with default options")
+plot(b, pages=1, residuals=TRUE, pch=16, cex=0.5, rug=FALSE,
+     shade.col=scales::alpha("lightblue", 0.5),
+     scheme=1, main="A gam fit with default options")
 dev.off()
 
 ## run some basic model checks, including checking
@@ -23,12 +25,22 @@ bb <- gam(y~s(x1,k=100), data=dat)
 plot(bb, residuals=TRUE, pch=16, cex=0.5)
 dev.off()
 
+plotfit <- function(model) {
+    fit <- predict(model, se.fit=TRUE)
+    lower <- fit$fit - fit$se.fit
+    upper <- fit$fit + fit$se.fit
+    polygon(x=c(dat$x1, rev(dat$x1)), y=c(lower, rev(upper)),
+            col=scales::alpha("lightblue", 0.5), border="lightblue")
+    lines(dat$x1, fit$fit, col="darkblue")
+}
+
 spline.basis <- as.data.frame(model.matrix(b))
-glm.out <- glm(dat$y ~ ., data=spline.basis)
+dat2 <- dat
+dat2$y <- dat2$y - mean(dat2$y)
+glm.out <- glm(dat2$y ~ 0 + ., data=spline.basis)
 x <- seq(from=0, to=1, length=101)
 png("gam-nonsmoothed.png")
-plot(y ~ x1, data=dat, pch=16, cex=0.5)
-ord <- order(dat$x1)
-lines(dat$x1[ord], fitted(glm.out)[ord])
+plot(y ~ x1, data=dat2, pch=16, cex=0.5, ylab="s(x1, 9)", main="An unpenalized spline fit")
+plotfit(glm.out)
 dev.off()
 
